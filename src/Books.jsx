1eb28ebec7138423cb0f08fbee54bbc187e698e7
Unais <unaissk1234@gmail.com>
2024-09-07 21:08:28 +0530

    added filter on click on console

--- src/Books.jsx
+++ src/Books.jsx
@@ -1,32 +1,40 @@
 import books from './book';
-
+import "./App.css"
 const BookList = () => {
-    return (
-      <div className="bookContainer">
-        {books.map((item,index) => {
+
+  const getBook = (id) => {
+    const book = books.find((item) => item.id == id
+    );
+    console.log(book.title);
+
+  }
+
+  return (
+    <div className="bookContainer">
+      {books.map((item) => {
         //   const { title, author, img } = item;
-  
-          return <Book {...item}  key={index} />
-        })}
-  
-      </div>
-    )
-  
-  
-  }
-  
-  const Book = (props) => {
-    const { title, img, author } = props;
-    return (
-      <div className="book-card">
-  
-        <h2 className="title">{title}</h2>
-  
-  
-        <img src={img} alt={title} />
-        <h3 className="author-name">{author}</h3>
-      </div>
-    )
-  
-  }
-  export default BookList;
+
+        return <Book {...item} key={item.id} getBook={getBook} />
+      })}
+
+    </div>
+  )
+
+
+}
+
+const Book = (props) => {
+  const { title, img, author,id ,getBook} = props;
+  return (
+    <div className="book-card">
+      <img src={img} alt={title} />
+      <h2 className="title">{title}</h2>
+      {/* <button className='btn' onClick={()=>console.log(title)}>Console Title</button> */}
+      {/* button directly hold the refrence to component independently ,while we have to struggle in vanilla Js */}
+      <h3 className="author-name">{author}</h3>
+      <button className="btn" onClick={()=>getBook(id)}>SHOW IN CONSOLE</button>
+    </div>
+  )
+
+}
+export default BookList;
--- src/book.js
+++ src/book.js
@@ -1,35 +1,43 @@
 const books = [
     {
+      id:1,
       title: "I don't love you anymore",
       author: "rithvik singh",
       img: "/book 1.jpg"
     }, {
+      id:2,
       title: "The Golden Road",
       author: "william delrimple",
       img: "/book 2.jpg"
     }, {
+      id:3,
       title: "atomic habits",
       author: "james clear",
       img: "/book 3.jpg"
     }, {
+      id:4,
       title: "dopamine detox",
       author: "thibaut morisse",
       img: "/book 4.jpg"
     }, {
+      id:5,
       title: "The power of your subconscious mind",
       author: "joseph murphy",
       img: "/book 5.jpg"
     }, {
+      id:6,
       title: "The power of your subconscious mind edition 2",
       author: "joseph murphy",
       img: "/book 6.jpg"
     },
      {
+      id:7,
       title: "1001 ultimate brain booster",
       author: "team pegasus",
       img: "/book 7.jpg"
     },
     {
+      id:8,
      title: "oxford student atlas for India",
      author: "oxford university press",
      img: "/book 8.jpg"
